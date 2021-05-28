fdddb72f155b20787feb046e936b6409c49bce05
Surya <surya.sendhilraj@gmail.com>
2021-05-27 21:22:13 -0400

    entry page works (1/7)

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -4,7 +4,15 @@ from . import util
 
 
 def index(request):
-    return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
-    })
+    return render(request, "encyclopedia/index.html", {"entries": util.list_entries()})
 
+
+def display_entry(request, name):
+    content = util.get_entry(name)
+    if content is not None:
+        return render(
+            request,
+            "encyclopedia/display_entry.html",
+            {"name": name, "content": content},
+        )
+    return render(request, "encyclopedia/error.html", {"name": name})

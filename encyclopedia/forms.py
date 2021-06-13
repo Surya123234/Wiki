67ba88faf4768fb785258eae3c4967cf947d1863
Surya <surya.sendhilraj@gmail.com>
2021-06-13 17:55:34 -0400

    bootstrapify

--- encyclopedia/forms.py
+++ encyclopedia/forms.py
@@ -2,5 +2,9 @@ from django import forms
 
 
 class newPageForm(forms.Form):
-    name = forms.CharField(label="Page Name:")
-    content = forms.CharField(label="Content:", widget=forms.Textarea)
+    name = forms.CharField(
+        label="Page Name:", widget=forms.TextInput(attrs={"class": "form-control"})
+    )
+    content = forms.CharField(
+        label="Content:", widget=forms.Textarea(attrs={"class": "form-control"})
+    )
